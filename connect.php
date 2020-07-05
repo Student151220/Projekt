62dcf3038b86bcc252aa68e2afdba4ebc0f6e330
Student151220 <66437520+Student151220@users.noreply.github.com>
2020-07-05 12:39:18 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,6 +1,12 @@
 <?php
-	$host = 'localhost';
-	$db_user = 'biblioteka_user';
-	$db_password = "123";
-	$db_name = "biblioteka";
+// 	$host = 'localhost';
+// 	$db_user = 'biblioteka_user';
+// 	$db_password = "123";
+// 	$db_name = "biblioteka";
+
+// heroku
+	$host = 'zpfp07ebhm2zgmrm.chr7pe7iynqr.eu-west-1.rds.amazonaws.com';
+	$db_user = 'wrwyxwdgyue3dik6';
+	$db_password = "l4d8mvqubuhgl6d1";
+	$db_name = "dti4drw2z0cfm7c6";
 ?>
